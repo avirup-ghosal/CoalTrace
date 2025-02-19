3128d2e2e23033de2fd0be80bded86cd383912f9
avirup-ghosal <avirupghosal2023@gmail.com>
2025-02-19 21:37:37 +0530

    Fixed import statement

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,12 +1,12 @@
 import Hero from "../components/Hero"
-import logo from "C:/Users/amita/OneDrive/Desktop/Projects/NITR/CoalTrace/frontend/src/Carbon.jpeg"
+// import logo from "CoalTrace/frontend/public/Carbon.jpeg"
 
 export default function Landing(){
     return(
         <div className="bg-gray-800 min-h-screen flex justify-center items-center">
             <div className="flex flex-row justify-center bg-gray-900 drop-shadow-3xl border-2 hover:border-[#26de9a]">
             <div className="flex justify-center items-center"><Hero></Hero></div>
-            <div className="hidden md:block"><img src={logo} alt="Logo" /></div>
+            <div className="hidden md:block"><img src="/Carbon.jpeg" alt="Logo" /></div>
             </div>
             
         </div>
