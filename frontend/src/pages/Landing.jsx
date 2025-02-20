b999c3049558370c29ed4120601dc773d7c31bb5
avirup-ghosal <avirupghosal2023@gmail.com>
2025-02-20 11:48:42 +0530

    fixing hero page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -6,7 +6,7 @@ export default function Landing(){
         <div className="bg-gray-800 min-h-screen flex justify-center items-center">
             <div className="flex flex-row justify-center bg-gray-900 drop-shadow-3xl border-2 hover:border-[#26de9a]">
             <div className="flex justify-center items-center"><Hero></Hero></div>
-            <div className="hidden md:block"><img src="/Carbon.jpeg" alt="Logo" /></div>
+            <div className="hidden md:block"><img className="h-full" src="/Carbon.jpeg" alt="Logo" /></div>
             </div>
             
         </div>
